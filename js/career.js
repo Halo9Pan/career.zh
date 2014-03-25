a4962050b1e9dd132dcbbf7264b38165def63353
Halo9Pan <halo9apn@gmail.com>
2014-03-26 00:23:50 +0800

    Update javascript

--- js/career.js
+++ js/career.js
@@ -1,35 +1,38 @@
-(function ( d, w ) {
-    'use strict';
-    
-    var skillElements = function() {
-    	var elements = d.querySelectorAll('div.skill');
-    	var skills = [].slice.call(elements);
-    	return skills;
-    };
-    
-    var init = function() {
-    	var elements = skillElements();
-    	for (var i in elements){
-    		var skill = elements[i];
-    		var arrow = d.createElement('div'),
-    		arrowUp = d.createElement('div'),
-    		arrowHandle = d.createElement('div');
-    		arrow.classList.add('arrow');
-    		arrowUp.classList.add('arrow-up');
-    		arrowHandle.classList.add('arrow-handle');
-       		arrow.appendChild(arrowUp);
-       		arrow.appendChild(arrowHandle);
-       		skill.appendChild(arrow);
-       		skill.parentNode.addEventListener("impress:stepenter", function (event) {
-       			var skill = event.target.querySelector('div.skill');
-       			var arrow = event.target.querySelector('div.arrow');
-       			var grow = skill.dataset['grow'];
-       			var start = skill.dataset['start'] || 0;
-       			arrow.style.height = parseInt(grow) * 100 + 'px';;
-       			arrow.style.bottom = parseInt(start) * 100 + 'px';;
-            }, false);
-    	}
-    };
-    init();
-    
+(function(d, w) {
+  'use strict';
+
+  var skillElements = function() {
+    var elements = d.querySelectorAll('div.skill');
+    var skills = [].slice.call(elements);
+    return skills;
+  };
+
+  var init = function() {
+    var elements = skillElements();
+    for (var i in elements) {
+      var skill = elements[i];
+      var arrow = d.createElement('div'),
+        arrowUp = d.createElement('div'),
+        arrowHandle = d.createElement('div');
+      arrow.classList.add('arrow');
+      arrowUp.classList.add('arrow-up');
+      arrowHandle.classList.add('arrow-handle');
+      arrow.appendChild(arrowUp);
+      arrow.appendChild(arrowHandle);
+      skill.appendChild(arrow);
+      skill.parentNode.addEventListener("impress:stepenter", grow, false);
+    }
+  };
+
+  var grow = function(e) {
+    var skill = event.target.querySelector('div.skill');
+    var arrow = event.target.querySelector('div.arrow');
+    var grow = skill.dataset['grow'];
+    var start = skill.dataset['start'] || 0;
+    arrow.style.height = parseInt(grow) * 100 + 'px';;
+    arrow.style.bottom = parseInt(start) * 100 + 'px';;
+  };
+
+  init();
+
 })(document, window);
